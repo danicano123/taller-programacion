0c2562fb28fc9cced755abe5186a6f4cdc67ab23
CarolinaGXx <carogt38@gmail.com>
2024-09-25 11:34:44 -0500

    feat: Model Product full

---
+++ Domain/NormativeAspects/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace programming_work_backend.Domain.Product.Models;
+
+public class Product
+{
+
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string Stock { get; set; }
+    public string UnitValue { get; set; }
+
+}
