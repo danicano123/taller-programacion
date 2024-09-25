15f362177313e8cef870a6d4cd14694c712f865f
CarolinaGXx <carogt38@gmail.com>
2024-09-25 11:35:12 -0500

    feat: model factura full

---
+++ Domain/NormativeAspects/Models/Invoice.cs
@@ -0,0 +1,10 @@
+namespace programming_work_backend.Domain.Invoice.Models;
+
+public class Invoice
+{
+
+    public string Date { get; set; }
+    public string Number { get; set; }
+    public string Total { get; set; }
+
+}
